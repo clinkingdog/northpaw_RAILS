0a9ed0fbca2235991dd1f557bcc0d1ed40e7ecc1
Nick Golding <clinkingdog@gmail.com>
2013-03-25 22:01:29 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Northpaw::Application.config.secret_token = 'd2a7a50773c99f35fe1f96223c41e1c6a1749f0c0b5b0f5e2f02195529395ff7fbc2b3c107ed422a4689b7b9f549f140a925b1d20d5b0a2d3bb123fca75791f6'
