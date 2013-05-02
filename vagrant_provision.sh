44889fe247404fca43eb0848210d13fb69e822f4
Nick Golding <clinkingdog@gmail.com>
2013-05-02 22:26:18 +0100

    Add git st and co aliases to vagrant provision

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -6,3 +6,5 @@ apt-get -y install nodejs
 bundle install --without production
 echo alias rails=\'bundle exec rails\' >> ~vagrant/.bash_aliases
 echo alias npsrv=\'bundle exec rails server\' >> ~vagrant/.bash_aliases
+git config -f ~vagrant/.gitconfig alias.st status
+git config -f ~vagrant/.gitconfig alias.co checkout
