dea32bf1826603847d66ee16dfd937f97f05a117
Nick Golding <clinkingdog@gmail.com>
2013-05-02 20:46:41 +0100

    prevent bundle install from installing production gems

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -3,6 +3,6 @@
 export PATH=/home/vagrant/.rbenv/shims:/home/vagrant/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin
 cd /vagrant
 apt-get -y install nodejs
-bundle install
+bundle install --without production
 echo alias rails=\'bundle exec rails\' >> ~vagrant/.bash_aliases
 echo alias npsrv=\'bundle exec rails server\' >> ~vagrant/.bash_aliases
