dcd5c90a9a75fa1a0722577b1e8a7ab5b5aca74a
Nick Golding <clinkingdog@gmail.com>
2014-03-27 20:07:05 +0000

    Attempt to narrow down failing sign-in in unit tests

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 include Devise::TestHelpers
 
-describe "Reports Controller", :type => :controller do
-  let(:user) {
-    FactoryGirl.create(
-      :user,
-      :email => 'test@test.test',
-      :password => 'testtest',
-      :confirmed_at => '2000-01-01')
-  }
+describe ReportsController, :type => :controller do
 
   describe "GET /reports" do
     # TODO check contexts
@@ -19,15 +12,21 @@ describe "Reports Controller", :type => :controller do
       # end
     # end
     
-    before(:each) do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in user
-    end
+    # before(:each) do
+    # end
     
     it "returns the page" do
+      # QQ PROBABLY REMOVE @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user, :email => 'test2@test.test', :password => 'testtest', :confirmed_at => Time.now)
+      sign_in :user, user
+      # QQ DEBUG REMOVE
+      p subject.current_user
+
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get "/reports"
+
       response.status.should eq(200)
+      # QQ response.should be_success
     end
   end
 end
